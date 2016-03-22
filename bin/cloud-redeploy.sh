2395a6abe622e7ff9b6ee9d14f00987feb16c05f
Federico Gimenez <fgimenez@coit.es>
2016-03-22 16:26:06 +0100

    remove dangling containers, images and volumes

--- bin/cloud-redeploy.sh
+++ bin/cloud-redeploy.sh
@@ -11,3 +11,7 @@ docker-compose -f ./config/jenkins/cluster.yml down
 docker-compose -f ./config/jenkins/cluster.yml up -d
 
 . ./bin/secrets/init-credentials.sh
+
+docker rm $(docker ps --no-trunc -aq)
+docker rmi $(docker images --filter dangling=true --quiet)
+docker volume rm $(docker volume ls -qf dangling=true)
