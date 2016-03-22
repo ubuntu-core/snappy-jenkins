53c4fa5ee72efb271b9fae22d8774cf0a90f41dc
Federico Gimenez <fgimenez@coit.es>
2016-03-22 17:29:20 +0100

    added comment to command for removing containers

--- bin/cloud-redeploy.sh
+++ bin/cloud-redeploy.sh
@@ -12,6 +12,9 @@ docker-compose -f ./config/jenkins/cluster.yml up -d
 
 . ./bin/secrets/init-credentials.sh
 
+# remove non-running containers (if they are running this command gives an error)
+# --no-trunc prevents id collisions by showing the complete field. -a shows all
+# containers, not only running ones. -q only outputs the numeric id field
 docker rm $(docker ps --no-trunc -aq)
 docker rmi $(docker images --filter dangling=true --quiet)
 docker volume rm $(docker volume ls -qf dangling=true)
