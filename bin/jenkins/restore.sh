315d776c45cecdc17899175efc9aeb961ac785ee
Federico Gimenez <fgimenez@coit.es>
2016-04-11 09:53:53 +0200

    added comment about copying backup to home instead of tmp

--- bin/jenkins/restore.sh
+++ bin/jenkins/restore.sh
@@ -17,6 +17,8 @@ machine_name="$NAME-$ENV"
 
 eval $(docker-machine env "$machine_name")
 
+# copy to /home/ubuntu, in the local deployments /tmp is a symlink and docker-machine scp
+# complains about copying to it
 docker-machine ssh "$machine_name" sudo rm -rf /home/ubuntu/backup.tar.gz
 docker-machine scp "$BACKUP_FILE" "$machine_name":/home/ubuntu/backup.tar.gz
 docker-machine ssh "$machine_name" sudo docker run --rm --volumes-from jenkins_jenkins-master-service_1 -v /home/ubuntu:/backup ubuntu:xenial tar xvfz /backup/backup.tar.gz
