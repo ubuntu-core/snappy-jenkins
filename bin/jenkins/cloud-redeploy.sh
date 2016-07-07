a2ddc453b8a1af02cf2cbaa2ac825d0f32494af8
Federico Gimenez <fgimenez@coit.es>
2016-07-07 16:09:03 +0200

    point the docker client to the cluster before calling cluster-related commands

--- bin/jenkins/cloud-redeploy.sh
+++ bin/jenkins/cloud-redeploy.sh
@@ -2,17 +2,14 @@
 
 set -x
 
-. ./bin/jenkins/common.sh
 . ./bin/cloud-common.sh
 
-machine_name=$(swarm_master_name)
-
-eval $(docker-machine env --swarm "$machine_name")
-
+eval $(docker-machine env --swarm "$(swarm_master_name)")
 docker-compose -f ./config/jenkins/cluster.yml pull
 
 . ./bin/jenkins/backup.sh
 
+eval $(docker-machine env --swarm "$(swarm_master_name)")
 docker-compose -f ./config/jenkins/cluster.yml down
 
 docker-compose -f ./config/jenkins/cluster.yml up -d
@@ -26,6 +23,7 @@ docker-compose -f ./config/jenkins/cluster.yml scale jenkins-slave-xenial=5
 # remove non-running containers (if they are running this command gives an error)
 # --no-trunc prevents id collisions by showing the complete field. -a shows all
 # containers, not only running ones. -q only outputs the numeric id field
+eval $(docker-machine env --swarm "$(swarm_master_name)")
 docker rm $(docker ps --no-trunc -aq)
 docker rmi $(docker images --filter dangling=true --quiet)
 docker volume rm $(docker volume ls -qf dangling=true)
